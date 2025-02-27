a4b9ed277bb9b8c985f864ab8354db7f22592317
Nataliia-Panchoshna <netishi87@gmail.com>
2025-02-27 19:21:10 +0000

    Deploying to gh-pages from @ Nataliia-Panchoshna/goit-js-hw-11@ca0733b428e9759f8ae5992d2c67d42e064ae618 🚀

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import{a as f,S as m,i}from"./assets/vendor-KnZd4sWe.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const y="49078062-013dc20f7945f56078ec7160a",d="https://pixabay.com/api/";async function g(o){try{return(await f.get(d,{params:{key:y,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}})).data.hits}catch(t){return console.error("Error fetching images:",t),[]}}const p=document.querySelector(".gallery");let l;function h(o){p.innerHTML=o.map(({webformatURL:t,largeImageURL:s,tags:n,likes:e,views:r,comments:a,downloads:u})=>`<li class="gallery-item">
-      <a href="${s}" class="gallery-link">
+      <a href="${s}"class="gallery-link">
         <img src="${t}" alt="${n}" />
-        <div class="info">
+        <div class="grid">
           <p>Likes: ${e}</p>
           <p>Views: ${r}</p>
           <p>Comments: ${a}</p>
